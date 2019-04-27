f040e692c3d6f3051dfcec0ba90e3bb7b1e3095c
sunilvhiremath <sunilvhiremath123@gmail.com>
2019-04-27 16:56:33 +0530

    seleum

--- demo 2/src/d1/Sun.java
+++ demo 2/src/d1/Sun.java
@@ -1,18 +1,38 @@
 package d1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByName;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+
+
+
+
 
 public class Sun {
 	@Test
-	public static void ss() {
-		// TODO Auto-generated method stub
-
+	public static void main() {
+		System.setProperty("webdriver.chrome.driver","H:\\Automation\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("http://www.google.co.in");
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys("fb");
+	    element.submit();
+		
+       
 		System.out.println("Hello ");
+		
+		
 	}
+
+		
 	
-	@Test
-	public void main() {
-		System.out.println("Hello world");
-	}
+
 
 }
