e532f9bc57f6be099613b0ce17df70312ab15948
sunilvhiremath <sunilvhiremath123@gmail.com>
2019-04-27 13:50:25 +0530

    sd

--- demo 2/src/d1/Sun.java
+++ demo 2/src/d1/Sun.java
@@ -5,7 +5,7 @@ public class Sun {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello worl");
+		System.out.println("Hello wol");
 	}
 
 }
